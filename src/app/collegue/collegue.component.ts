0464f57946e45aeacbd1a75b85921f3f09b2e906
Khalil HIMET <khalil.himet@outlook.com>
2020-07-30 14:26:12 +0200

    TP 02 Front - Angular

---
+++ src/app/collegue/collegue.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Collegue } from '../models/Collegue';
+
+@Component({
+  selector: 'app-collegue',
+  templateUrl: './collegue.component.html',
+  styleUrls: ['./collegue.component.css']
+})
+export class CollegueComponent implements OnInit {
+  @Input() col: Collegue;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  clickCreerClient(): void {
+    console.log('Créer un nouveau collègue');
+  }
+
+  clickModifierClient(): void {
+    console.log('Modification du collègue');
+  }
+
+}
---
+++ src/app/mock/collegues.mock.ts
@@ -0,0 +1,4 @@
+import { Collegue } from '../models/Collegue';
+
+export const collegueMock = new Collegue('abcd', 'Dupont', 'Marc', 'dupontmarc@gmail.com', new Date(1975, 8, 15),
+'https://k2partnering.com/wp-content/uploads/2016/05/Person.jpg');
